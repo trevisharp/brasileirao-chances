284ec8ee6705b37c47897e0b4d47de48f6c518ac
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-09-08 03:16:40 -0300

    Get data page

--- Program.cs
+++ Program.cs
@@ -1,16 +1,122 @@
-namespace brasileirao_chances;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
 
-static class Program
+try
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    await generate();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+async Task generate()
+{
+    var page = await getPage();
+    await extractTables(page);
+}
+
+async Task extractTables(string page)
+{
+    var days = page.Split("Dia de Jogo");
+
+    await Task.Factory.StartNew(() => 
+        Parallel.ForEach(days[1..], day =>
+        {
+            processDay(day);
+        })
+    );
+}
+
+void processDay(string dayHtml)
+{
+    var start = 0;
+    var end = dayHtml.IndexOf("</tr", start + 1);
+    
+    for (int i = 0; i < 10; i++)
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        start = dayHtml.IndexOf("<tr", end + 1);
+        end = dayHtml.IndexOf("</tr", start + 1);
+        if (start == -1 || end == -1)
+            break;
+        
+        var table = dayHtml.Substring(start, end - start);
+
+        System.Console.WriteLine(table);
+    }
+}
+
+async Task<string> getPage()
+{
+    var hasCache = File.Exists("page.html");
+    var hasInternet = await testConnectivity();
+
+    if (!hasInternet && hasCache)
+        return await loadPageFromCache();
+    
+    if (hasInternet && !hasCache)
+        return await getOnlineAndSave();
+    
+    if (!hasInternet && !hasCache)
+        throw new Exception("We dont has internet and dont exist a cache of data.");
+
+    if (isCacheRecently())
+        return await loadPageFromCache();
+    
+    return await getOnlineAndSave();
+}
+
+bool isCacheRecently()
+{
+    var lastWrite = File.GetLastWriteTime("page.html");
+    var today = DateTime.Today;
+    var diff = lastWrite - today;
+    return diff.TotalDays < 1;
+}
+
+async Task<string> getOnlineAndSave()
+{
+    var page = await getOnlinePage();
+
+    if (!File.Exists("page.html"))
+        File.CreateText("page.html").Close();
+
+    await File.WriteAllTextAsync("page.html", page);
+
+    return page;
+}
+
+async Task<string> loadPageFromCache()
+{
+    var content = await File.ReadAllTextAsync("page.html");
+    
+    return content;
+}
+
+async Task<string> getOnlinePage()
+{
+    const string dataPath = "https://footystats.org/pt/brazil/serie-a/fixtures";
+
+    var http = new HttpClient();
+    var response = await http.GetAsync(dataPath);
+
+    if (!response.IsSuccessStatusCode)
+        throw new Exception($"Request Failed with {response.StatusCode} status code.");
+
+    var content = await response.Content.ReadAsStringAsync();
+    return content;
+}
+
+async Task<bool> testConnectivity()
+{
+    var ping = new Ping();
+    var result = await ping.SendPingAsync(
+        "google.com", 3000, 
+        new byte[32], 
+        new PingOptions()
+    );
+    return result.Status == IPStatus.Success;
 }
